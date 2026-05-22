0f35a56c706f56d9be9147260a1ada6eff16081d
Alex1under <alexanderanufriev111@gmail.com>
2026-05-22 12:45:00 +0200

    way pre end

--- components/ways.php
+++ components/ways.php
@@ -1,5 +1,5 @@
 <section id="ways" class="section-container bg-image-container">
-
+    <img class="bg-image" src="./assets/images/bg-ways.png" alt="">
     <div class="container bg-image-container">
 
         <div class="title-block">
@@ -8,101 +8,241 @@
         </div>
 
         <div class="content">
-            
-            <div class="ways-wrapper">
-                <img class="ways-circle" src="./assets/images/marketing-circle.png" alt="">
-                <div class="place-for-way side-way">
-                    <div class="way pink-way">
-                        <div class="head-way-wrapper">
-                            <object class="svg-way svg-way-pink" data="./assets/icons/way-pink.svg" type="image/svg+xml"></object>
-                            <div class="head-way">
-                                <div class="folder-block"></div>Research & Analytics
+            <div class="desktop">
+                <div class="ways-wrapper">
+                    <img class="ways-circle" src="./assets/images/marketing-circle.png" alt="">
+                    <div class="place-for-way side-way">
+                        <div class="way pink-way">
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Audience Analytics</div>
                             </div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Audience Analytics</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Target Persona Creation</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Product Marketing Research</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Business Process Research</div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Target Persona Creation</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Product Marketing Research</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Business Process Research</div>
+                            </div>
+                            <div class="head-way-wrapper">
+                                <object class="svg-way svg-way-pink" data="./assets/icons/way-pink.svg" type="image/svg+xml"></object>
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="place-for-way">
-                    <div class="way purple-way">
-                        <div class="head-way-wrapper">
-                            <object class="svg-way svg-way-purple" data="./assets/icons/way-purple.svg" type="image/svg+xml"></object>
-                            <div class="head-way">
-                                <div class="folder-block"></div>Research & Analytics
+                    <div class="place-for-way">
+                        <div class="way purple-way">
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Audience Analytics</div>
                             </div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Audience Analytics</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Target Persona Creation</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Product Marketing Research</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Business Process Research</div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Target Persona Creation</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Product Marketing Research</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Business Process Research</div>
+                            </div>
+                            <div class="head-way-wrapper">
+                                <object class="svg-way svg-way-purple" data="./assets/icons/way-purple.svg" type="image/svg+xml"></object>
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="place-for-way">
-                    <div class="way orange-way">
-                        <div class="head-way-wrapper">
-                            <object class="svg-way svg-way-orange" data="./assets/icons/way-orange.svg" type="image/svg+xml"></object>
-                            <div class="head-way">
-                                <div class="folder-block"></div>Research & Analytics
+                    <div class="place-for-way">
+                        <div class="way orange-way">
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Audience Analytics</div>
                             </div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Audience Analytics</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Target Persona Creation</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Product Marketing Research</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Business Process Research</div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Target Persona Creation</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Product Marketing Research</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Business Process Research</div>
+                            </div>
+                            <div class="head-way-wrapper">
+                                <object class="svg-way svg-way-orange" data="./assets/icons/way-orange.svg" type="image/svg+xml"></object>
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="place-for-way side-way">
-                    <div class="way blue-way">
-                        <div class="head-way-wrapper">
-                            <object class="svg-way svg-way-blue" data="./assets/icons/way-blue.svg" type="image/svg+xml"></object>
-                            <div class="head-way">
-                                <div class="folder-block"></div>Research & Analytics
+                    <div class="place-for-way side-way">
+                        <div class="way blue-way">
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Audience Analytics</div>
                             </div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Audience Analytics</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Target Persona Creation</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Product Marketing Research</div>
-                        </div>
-                        <div class="option-way-wrapper">
-                            <div class="option-way">Business Process Research</div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Target Persona Creation</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Product Marketing Research</div>
+                            </div>
+                            <div class="option-way-wrapper">
+                                <div class="option-way">Business Process Research</div>
+                            </div>
+                            <div class="head-way-wrapper">
+                                <object class="svg-way svg-way-blue" data="./assets/icons/way-blue.svg" type="image/svg+xml"></object>
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="mobile">
+                <img class="ways-circle" src="./assets/images/marketing-circle.png" alt="">
+                <div class="ways-wrapper">
+                    <div class="place-for-way">
+                        <div class="way pink-way">
+                            <div class="head-way-wrapper">
+                                <!-- <object class="svg-way svg-way-pink" data="./assets/icons/way-pink.svg" type="image/svg+xml"></object> -->
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
+
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-left" width="7" height="51">
+                                    <use href="#way-left"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Audience Analytics</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Target Persona Creation</div>
+                                </div>
+                            </div>   
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-right" width="7" height="51">
+                                    <use href="#way-right"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Product Marketing Research</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Business Process Research</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="place-for-way">
+                        <div class="way purple-way">
+                            <div class="head-way-wrapper">
+                                <!-- <object class="svg-way svg-way-purple" data="./assets/icons/way-purple.svg" type="image/svg+xml"></object> -->
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-left" width="7" height="51">
+                                    <use href="#way-left"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Audience Analytics</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Target Persona Creation</div>
+                                </div>
+                            </div>   
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-right" width="7" height="51">
+                                    <use href="#way-right"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Product Marketing Research</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Business Process Research</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="place-for-way">
+                        <div class="way orange-way">
+                            <div class="head-way-wrapper">
+                                <!-- <object class="svg-way svg-way-orange" data="./assets/icons/way-orange.svg" type="image/svg+xml"></object> -->
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-left" width="7" height="51">
+                                    <use href="#way-left"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Audience Analytics</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Target Persona Creation</div>
+                                </div>
+                            </div>   
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-right" width="7" height="51">
+                                    <use href="#way-right"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Product Marketing Research</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Business Process Research</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="place-for-way">
+                        <div class="way blue-way">
+                            <div class="head-way-wrapper">
+                                <!-- <object class="svg-way svg-way-blue" data="./assets/icons/way-blue.svg" type="image/svg+xml"></object> -->
+                                <div class="head-way">
+                                    <div class="folder-block"></div>Research & Analytics
+                                </div>
+                            </div>
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-left" width="7" height="51">
+                                    <use href="#way-left"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Audience Analytics</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Target Persona Creation</div>
+                                </div>
+                            </div>   
+                            <div class="way-column">
+                                <svg class="svg-way svg-way-right" width="7" height="51">
+                                    <use href="#way-right"></use>
+                                </svg>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Product Marketing Research</div>
+                                </div>
+                                <div class="option-way-wrapper">
+                                    <div class="option-way">Business Process Research</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
