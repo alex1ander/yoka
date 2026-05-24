bd1bf3c1534d8117457ab00edd0e92b6f4e605b8
Alex1under <alexanderanufriev111@gmail.com>
2026-05-24 14:15:31 +0200

    finish all blcoks

--- assets/scripts/growth.js
+++ assets/scripts/growth.js
@@ -1,11 +1,23 @@
-const creategrowthSwiper = new Swiper(".growth-swiper", {
-    slidesPerView: "auto",
+const creategrowthSwiper = new Swiper(".growth-support-swiper", {
+    slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
-    centeredSlides: true,
     navigation: {
         nextEl: ".growth-swiper-button-next",
         prevEl: ".growth-swiper-button-prev",
-        
     },
 });
+
+const creategrowthMainSwiper = new Swiper(".growth-main-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+        nextEl: ".growth-main-swiper-button-next",
+        prevEl: ".growth-main-swiper-button-prev",
+    },
+});
+
+// Связка двух слайдеров
+creategrowthSwiper.controller.control = creategrowthMainSwiper;
+creategrowthMainSwiper.controller.control = creategrowthSwiper;
--- components/growth.php
+++ components/growth.php
@@ -10,57 +10,96 @@
         </div>
     </div>
 
-    <div class="container container-full bg-image-container container-p0">
-        <div class="content">
-
-            <div class="place-for-slider anim-place">
+    <div class="container">
+            <div class="place-for-slider">
                 
-                <div class="swiper growth-swiper">
-                    <div class="swiper-wrapper">
-                        <!--                         
-                            <div class="card-software">
-                                <img class="bg-image figure" src="./assets/images/bg-software.png" alt="">
 
-                                <div class="content-card">
-                                    <span class="big-numer">2.5<span class="small-text">x</span></span>
-                                    <span class="big-text">GROWTH</span>
-                                </div>
-                            </div> -->
-                        <?php for($i=0; $i< 4; $i++): ?>
-
-
+                <div class="slider-wrapper">
+                    <img class="bg-image desktop" src="./assets/images/bg-software.png" alt="">
+                    <div class="swiper growth-support-swiper">
+                        <div class="swiper-wrapper">
+                            <?php for($i= 0; $i < 5; $i++): ?>
                             <div class="swiper-slide">
-                                <div class="card-image">
-                                    <img src="./assets/images/card-growth-1.png" alt="">
+                                <div class="card-software">
+                                    <div class="part head-part">
+                                        <div class="short-info">
+                                            <span class="big-numer">2.5<span class="small-text">x</span></span>
+                                            <span class="big-text">GROWTH</span>
+                                        </div>
+
+
+                                        <div class="media">
+                                            <div class="social-links">
+                                                <a href="#" >
+                                                    <object data="./assets/icons/instagram.svg" type="image/svg+xml"></object>
+                                                </a>
+
+                                                <a href="#" >
+                                                    <object data="./assets/icons/facebook.svg" type="image/svg+xml"></object>
+                                                </a>
+                                            </div>
+                                            <div class="text">CPA $4.50</div>
+                                        </div>
+                                    </div>
+                                    <div class="part">
+                                        <h4 class="card-title">SOFTWARE & SAAS</h4>
+                                        <span class="card-sub-title">FinTech Mobile App</span>
+                                        <p class="text">Implemented precise audience filtration and disruptive video creatives to dramatically lower the cost per install and boost user lifetime value.</p>
+                                        <ul class="list">
+                                            <li>Conversion Rate: +45%</li>
+                                            <li>Timeframe: 6 Weeks</li>
+                                        </ul>
+                                    </div>
+
                                 </div>
                             </div>
-                            <div class="swiper-slide">
-                                <div class="card-image">
-                                    <img src="./assets/images/card-growth-2.png" alt="">
-                                </div>
-                            </div>
+                            <?php endfor; ?>
+                        </div>
 
-
-                        <?php endfor; ?>
                     </div>
+                </div>                
 
-                    <div class="growth-swiper-button-prev round-btn cp glass-effect">
+
+                <div class="tablet-wrapper">
+                    <div class="swiper growth-main-swiper">
+                        <div class="swiper-wrapper">
+                            <?php for($i= 0; $i < 5; $i++): ?>
+                                <div class="swiper-slide">
+                                    <img class="content-image" src="./assets/images/card-x.png" alt="">
+                                </div>
+                            <?php endfor; ?>
+                        </div>
+                    </div>
+                    <img class="border-image" src="./assets/images/bg-2.5x.png" alt="">
+                    <div class="growth-main-swiper-button-prev round-btn cp glass-effect mobile">
                         <svg width="24" height="24">
                             <use href="#arrow-right"></use>
                         </svg>
                     </div>
-                    <div class="growth-swiper-button-next round-btn cp glass-effect next-button">
+                    <div class="growth-main-swiper-button-next round-btn cp glass-effect next-button mobile">
                         <svg width="24" height="24">
                             <use href="#arrow-right"></use>
                         </svg>
                     </div>
                 </div>
+
+
+
+                <div class="growth-swiper-button-prev round-btn cp glass-effect desktop">
+                    <svg width="24" height="24">
+                        <use href="#arrow-right"></use>
+                    </svg>
+                </div>
+                <div class="growth-swiper-button-next round-btn cp glass-effect next-button desktop">
+                    <svg width="24" height="24">
+                        <use href="#arrow-right"></use>
+                    </svg>
+                </div>
+
+                
             </div>
 
-        </div>
     </div>
 </section>
 <script src="./assets/scripts/growth.js"></script>
-
-
 
